fcccc1a5c8b24f3d661c733eab60e1e6f6ad3795
chris <512364167@qq.com>
2020-07-12 18:39:45 +0800

    Initial commit

---
+++ src/main/java/com/chris/prac/concurrency/volatileDemo.java
@@ -0,0 +1,46 @@
+package com.chris.prac.concurrency;
+
+import java.util.concurrent.TimeUnit;
+
+public class volatileDemo {
+    final static int MAX = 5;
+    static volatile int INIT_VALUE = 0;
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            int local_value = INIT_VALUE;
+            while (local_value < MAX) {
+                if (local_value != INIT_VALUE) {
+                    System.out.println("Read:" + INIT_VALUE);
+                    local_value = INIT_VALUE;
+                }
+            }
+//            while (INIT_VALUE < MAX) {
+//                if (INIT_VALUE < MAX) {
+//                    System.out.println("Read:" + INIT_VALUE);
+//                }
+//            }
+        }, "Reader").start();
+
+        new Thread(() -> {
+            int local_value = INIT_VALUE;
+            while (local_value < MAX) {
+                System.out.println("Update:" + (++local_value));
+                INIT_VALUE = local_value;
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+//            while (INIT_VALUE < MAX) {
+//                System.out.println("Update:" + (++INIT_VALUE));
+//                try {
+//                    TimeUnit.SECONDS.sleep(2);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+        }, "Updater").start();
+    }
+}
